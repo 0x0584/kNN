7e1d7d821aef958a730d7a01e8b6a6883083cbde
Anas Rchid <rchid.anas@gmail.com>
2017-12-16 00:21:22 +0000

    Initial Commit

---
+++ funcs.php
@@ -0,0 +1,16 @@
+<?php
+define("BR", "<br />");
+
+function getdata($filename = "data.txt") {
+  $data = null; /* this is the filled data as form of array of arrays */
+  return $data;
+}
+
+function calcdistance() {
+  
+}
+
+function getNN() {
+  
+}
+?>
---
+++ kNN.php
@@ -0,0 +1,14 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 	  		  AUTHOR: ANAS RCHID                         *
+ *			 CREATED: 11 Dec 2017                        *
+ *                                                                   *
+ *	DESCRIPTION: Implementation de kNN avec plusieurs facteurs   *
+ *		     et chaque facteur a un poids different.         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* all the functions */
+require_once 'funcs.php';
+
+echo "this is a test".BR;
+?>
