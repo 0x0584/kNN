e97c9318f44cbf0806870a25b5b7b6bb8ad84fc2
Anas Rchid <rchid.anas@gmail.com>
2017-12-16 01:19:18 +0000

    read from file. fucking late

--- funcs.php
+++ funcs.php
@@ -1,16 +1,46 @@
 <?php
 define("BR", "<br />");
+define("HR", "<hr />");
+
+class DataPiece {
+    public $type; /* its type*/
+    public $f_list; /* factors list */
+
+    public function __construct($type, $f_list) {
+        $this->$type = $type;
+        $this->$f_list = $f_list;
+    }
+}
 
 function getdata($filename = "data.txt") {
-  $data = null; /* this is the filled data as form of array of arrays */
-  return $data;
+    $data[] = array(); /* as form of array of arrays */
+    $fh = fopen($filename, 'r');
+  
+    $n_factors = (int) fgets($fh);
+    
+    while ($line = fgets($fh)) {
+        $line = explode(':', $line);
+        $data[] = $line;
+    }
+
+    fclose($fh);
+  
+    foreach($data as $d) {
+        for($i = 0; $i < $n_factors; $i++) {
+            print "factor_$i: ".$d[$i]." ";
+        }
+      
+        echo BR;
+    }
+  
+    return $data;
 }
 
 function calcdistance() {
-  
+      
 }
 
 function getNN() {
-  
+      
 }
 ?>
--- kNN.php
+++ kNN.php
@@ -1,14 +1,16 @@
 <?php
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * 	  		  AUTHOR: ANAS RCHID                         *
- *			 CREATED: 11 Dec 2017                        *
+ *                		  AUTHOR: ANAS RCHID                         *
+ *                       CREATED: 11 Dec 2017                        *
  *                                                                   *
- *	DESCRIPTION: Implementation de kNN avec plusieurs facteurs   *
- *		     et chaque facteur a un poids different.         *
+ *      DESCRIPTION: Implementation de kNN avec plusieurs facteurs   *
+ *                   et chaque facteur a un poids different.         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /* all the functions */
 require_once 'funcs.php';
 
-echo "this is a test".BR;
+getdata();
+
+echo HR."this is a test";
 ?>
